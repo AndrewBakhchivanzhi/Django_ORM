580969c0aa9a39a290c3132f7f5fd58fcc6c18c8
Andrew <bakhcha_ml@mail.ru>
2024-01-03 17:20:06 +0300

    first_migrate

---
+++ catalog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='наименование')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='наименование')
+    description = models.TextField(max_length=250, verbose_name='описание')
+    image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price_for_one = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена за штуку')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    last_modified_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
